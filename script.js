ba86fc8ea77c4e04a59f61f1c72cb5b9ec136c57
Zach Coursey <zcoursey22@gmail.com>
2018-03-07 23:09:53 -0500

    Add core functionality

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $(function() {
 
+  var paused = false;
+  var onBreak = false;
+
   $('#addSessionTimeButton').click(function() {
     if ($('#sessionTime').text() < 60) {
       var sessionTime = $('#sessionTime').text();
       sessionTime++;
       $('#sessionTime').text(sessionTime);
+      $('#currentTimer').text('Session');
+      $('#timer')[0].style.color = 'black';
+      if (typeof playing == 'undefined' && paused == false) $('#timer').text(sessionTime);
     }
   });
 
@@ -13,6 +19,9 @@ $(function() {
       var sessionTime = $('#sessionTime').text();
       sessionTime--;
       $('#sessionTime').text(sessionTime);
+      $('#currentTimer').text('Session');
+      $('#timer')[0].style.color = 'black';
+      if (typeof playing == 'undefined' && paused == false) $('#timer').text(sessionTime);
     }
   });
 
@@ -21,6 +30,8 @@ $(function() {
       var breakTime = $('#breakTime').text();
       breakTime++;
       $('#breakTime').text(breakTime);
+      $('#currentTimer').text('Session');
+      $('#timer')[0].style.color = 'black';
     }
   });
 
@@ -29,19 +40,51 @@ $(function() {
       var breakTime = $('#breakTime').text();
       breakTime--;
       $('#breakTime').text(breakTime);
+      $('#currentTimer').text('Session');
+      $('#timer')[0].style.color = 'black';
+    }
+  });
+
+  $('#playButton').click(function() {
+    if (typeof playing == "undefined") {
+      $('#currentTimer').text('Session');
+      $('#timer')[0].style.color = 'black';
+      playing = setInterval(play, 1000);
+      paused = false;
     }
   });
 
   function play() {
-    console.log('timer started')
+    var timer = $('#timer').text();
+    if (timer > 0) {
+      timer--;
+      $('#timer').text(timer);
+      if (timer == 0 && onBreak === true) {
+        clearInterval(playing);
+        playing = undefined;
+        $('#timer')[0].style.color = 'red';
+        onBreak = false;
+      } else if (timer == 0) {
+        $('#currentTimer').text('Break');
+        $('#timer').text($('#breakTime').text());
+        onBreak = true;
+      }
+    }
   }
 
-  function pause() {
-    console.log('timer paused')
-  }
+  $('#pauseButton').click(function() {
+    if (typeof playing != "undefined") {
+      clearInterval(playing);
+      playing = undefined;
+      paused = true;
+    }
+  });
 
-  function reset() {
-    console.log('timer reset')
-  }
+  $('#resetButton').click(function() {
+    clearInterval(playing);
+    playing = undefined;
+    $('#timer').text($('#sessionTime').text());
+    paused = false;
+  });
 
 });
