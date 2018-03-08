c5e6c6b29161ee6f5227e223de3e1560f1eefb73
Zach Coursey <zcoursey22@gmail.com>
2018-03-08 00:29:31 -0500

    Format numbers

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
 $(function() {
 
+  var sessionLength = 1500;
+  var breakLength = 300;
+  var timeRemaining = sessionLength;
   var paused = false;
   var onBreak = false;
 
+  $('#sessionTime').text(numberFormat(sessionLength));
+  $('#breakTime').text(numberFormat(breakLength));
+  $('#timer').text(fullNumberFormat(timeRemaining));
+
+  function numberFormat(num) {
+    return Math.floor(num / 60);
+  }
+
+  function fullNumberFormat(num) {
+    var minutes = Math.floor(num / 60);
+    if (minutes < 10) minutes = '0' + minutes;
+    var seconds = Math.floor(num % 60);
+    if (seconds < 10) seconds = '0' + seconds;
+    return minutes + ':' + seconds
+  }
+
   $('#addSessionTimeButton').click(function() {
-    if ($('#sessionTime').text() < 60) {
-      var sessionTime = $('#sessionTime').text();
-      sessionTime++;
-      $('#sessionTime').text(sessionTime);
+    if (sessionLength < 3600) {
+      sessionLength += 60;
+      $('#sessionTime').text(numberFormat(sessionLength));
       $('#currentTimer').text('Session');
       $('#timer')[0].style.color = 'black';
-      if (typeof playing == 'undefined' && paused == false) $('#timer').text(sessionTime);
+      if (typeof playing == 'undefined' && paused == false) {
+        timeRemaining = sessionLength;
+        $('#timer').text(fullNumberFormat(timeRemaining));
+      }
     }
   });
 
   $('#removeSessionTimeButton').click(function() {
-    if ($('#sessionTime').text() > 1) {
-      var sessionTime = $('#sessionTime').text();
-      sessionTime--;
-      $('#sessionTime').text(sessionTime);
+    if (sessionLength > 60) {
+      sessionLength -= 60;
+      $('#sessionTime').text(numberFormat(sessionLength));
       $('#currentTimer').text('Session');
       $('#timer')[0].style.color = 'black';
-      if (typeof playing == 'undefined' && paused == false) $('#timer').text(sessionTime);
+      if (typeof playing == 'undefined' && paused == false) {
+        timeRemaining = sessionLength;
+        $('#timer').text(fullNumberFormat(timeRemaining));
+      }
     }
   });
 
   $('#addBreakTimeButton').click(function() {
-    if ($('#breakTime').text() < 30) {
-      var breakTime = $('#breakTime').text();
-      breakTime++;
-      $('#breakTime').text(breakTime);
+    if (breakLength < 1800) {
+      breakLength += 60;
+      $('#breakTime').text(numberFormat(breakLength));
       $('#currentTimer').text('Session');
       $('#timer')[0].style.color = 'black';
     }
   });
 
   $('#removeBreakTimeButton').click(function() {
-    if ($('#breakTime').text() > 1) {
-      var breakTime = $('#breakTime').text();
-      breakTime--;
-      $('#breakTime').text(breakTime);
+    if (breakLength > 60) {
+      breakLength -= 60;
+      $('#breakTime').text(numberFormat(breakLength));
       $('#currentTimer').text('Session');
       $('#timer')[0].style.color = 'black';
     }
@@ -48,25 +69,27 @@ $(function() {
   $('#playButton').click(function() {
     if (typeof playing == "undefined") {
       $('#currentTimer').text('Session');
-      $('#timer')[0].style.color = 'black';
+      $('#timer')[0].style.color = 'green';
       playing = setInterval(play, 1000);
       paused = false;
     }
   });
 
   function play() {
-    var timer = $('#timer').text();
-    if (timer > 0) {
-      timer--;
-      $('#timer').text(timer);
-      if (timer == 0 && onBreak === true) {
+    if (timeRemaining > 0) {
+      timeRemaining--;
+      $('#timer').text(fullNumberFormat(timeRemaining));
+      if (timeRemaining <= 5) $('#timer')[0].style.color = 'yellow';
+      if (timeRemaining == 0 && onBreak === true) {
         clearInterval(playing);
         playing = undefined;
         $('#timer')[0].style.color = 'red';
         onBreak = false;
-      } else if (timer == 0) {
+      } else if (timeRemaining == 0) {
+        $('#timer')[0].style.color = 'green';
         $('#currentTimer').text('Break');
-        $('#timer').text($('#breakTime').text());
+        timeRemaining = breakLength;
+        $('#timer').text(fullNumberFormat(timeRemaining));
         onBreak = true;
       }
     }
@@ -77,14 +100,17 @@ $(function() {
       clearInterval(playing);
       playing = undefined;
       paused = true;
+      $('#timer')[0].style.color = 'teal';
     }
   });
 
   $('#resetButton').click(function() {
     clearInterval(playing);
     playing = undefined;
-    $('#timer').text($('#sessionTime').text());
+    timeRemaining = sessionLength;
+    $('#timer').text(fullNumberFormat(timeRemaining));
     paused = false;
+    $('#timer')[0].style.color = 'black';
   });
 
 });
